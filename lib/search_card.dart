d2a770a6d7c1c901af721a55f584e7a219b59285
Rishita Singh <rishitas0605@gmail.com>
2024-09-15 02:07:50 +0530

    search

--- lib/search_card.dart
+++ lib/search_card.dart
@@ -1,18 +1,17 @@
 import 'package:flutter/material.dart';
 
 class SearchCard extends StatelessWidget {
-  const SearchCard(
-      {super.key,
-      required this.ontap,
-      required this.imageUrl,
-      required this.name,
-      required this.time1,
-      required this.author1});
+  const SearchCard({
+    super.key,
+    required this.ontap,
+    required this.disease,
+    required this.name,
+    required this.age,
+  });
   final VoidCallback ontap;
-  final String imageUrl;
+  final String disease;
   final String name;
-  final String time1;
-  final String author1;
+  final String age;
 
   @override
   Widget build(BuildContext context) {
@@ -26,81 +25,116 @@ class SearchCard extends StatelessWidget {
         decoration: BoxDecoration(
             color: const Color(0xFFe4f7ff),
             borderRadius: BorderRadius.circular(30)),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              width: 120,
-              height: 120,
-              decoration: BoxDecoration(
-                  color: const Color(0xFFe4f7ff),
-                  borderRadius: BorderRadius.circular(20)),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Row(
+        child: Expanded(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: 120,
+                height: 120,
+                decoration: BoxDecoration(
+                    color: const Color(0xFFe4f7ff),
+                    borderRadius: BorderRadius.circular(20)),
+                child: Expanded(
+                  child: Column(
                     mainAxisAlignment: MainAxisAlignment.start,
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Padding(
-                        padding: EdgeInsets.only(left: 10.0, top: 10.0),
-                        child: Icon(
-                          Icons.account_circle_rounded,
-                          size: 50.0,
-                          color: Colors.white,
+                      Expanded(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Padding(
+                              padding: EdgeInsets.only(left: 10.0, top: 10.0),
+                              child: Icon(
+                                Icons.account_circle_rounded,
+                                size: 50.0,
+                                color: Colors.white,
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 2.0,
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(left: 10.0, top: 20.0),
+                              child: Text(
+                                name,
+                                style: const TextStyle(
+                                    color: Colors.black, fontSize: 15.0),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                      SizedBox(
-                        width: 2.0,
-                      ),
                       Padding(
-                        padding: EdgeInsets.only(left: 10.0, top: 20.0),
+                        padding: const EdgeInsets.only(
+                          top: 10.0,
+                          left: 20.0,
+                        ),
                         child: Text(
-                          'Name',
-                          style: TextStyle(color: Colors.black, fontSize: 15.0),
+                          'Age: $age',
+                          style: const TextStyle(
+                              color: Colors.black,
+                              fontSize: 12.0,
+                              fontWeight: FontWeight.w400),
+                        ),
+                      ),
+                      Expanded(
+                        child: Row(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                left: 20.0,
+                              ),
+                              child: Text(
+                                disease,
+                                style: const TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     ],
                   ),
-                  const Padding(
-                    padding: EdgeInsets.only(
-                      left: 20.0,
-                    ),
-                    child: Text(
-                      'Age:',
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 12.0,
-                          fontWeight: FontWeight.w400),
-                    ),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.only(
-                      left: 20.0,
-                    ),
-                    child: Text(
-                      'DISEASE',
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 20.0, left: 150.0),
-                    child: Container(
-                      width: 50,
-                      height: 20,
-                      decoration: BoxDecoration(
-                          color: const Color(0xFF315ee7),
-                          borderRadius: BorderRadius.circular(2)),
-                    ),
-                  ),
-                ],
+                ),
               ),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.only(top: 80.0, left: 130.0),
+                child: Stack(
+                  children: [
+                    // The Container
+                    Container(
+                      width: 80,
+                      height: 30,
+                      decoration: BoxDecoration(
+                        color: const Color(0xFF315ee7),
+                        borderRadius: BorderRadius.circular(2),
+                      ),
+                    ),
+                    // The Text on top of the Container
+                    const Positioned.fill(
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Text(
+                          'VIEW',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 12.0, // Adjust as needed
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
