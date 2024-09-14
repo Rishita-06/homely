829c651e42b9929d1d4743177b2eb8e7f3723b64
Rishita Singh <rishitas0605@gmail.com>
2024-09-14 23:54:43 +0530

    card

--- lib/search_card.dart
+++ lib/search_card.dart
@@ -19,7 +19,9 @@ class SearchCard extends StatelessWidget {
     return InkWell(
       onTap: ontap,
       child: Container(
-        margin: const EdgeInsets.only(bottom: 15),
+        margin: const EdgeInsets.only(
+          bottom: 15,
+        ),
         padding: const EdgeInsets.all(10),
         decoration: BoxDecoration(
             color: const Color(0xFFe4f7ff),
@@ -73,29 +75,28 @@ class SearchCard extends StatelessWidget {
                           fontWeight: FontWeight.w400),
                     ),
                   ),
-                  Row(
-                    children: [
-                      const Padding(
-                        padding: EdgeInsets.only(
-                          left: 20.0,
-                        ),
-                        child: Text(
-                          'DISEASE',
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 20.0,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                      Container(
-                        width: 120,
-                        height: 120,
-                        decoration: BoxDecoration(
-                            color: const Color(0xFFe4f7ff),
-                            borderRadius: BorderRadius.circular(20)),
-                      ),
-                    ],
-                  )
+                  const Padding(
+                    padding: EdgeInsets.only(
+                      left: 20.0,
+                    ),
+                    child: Text(
+                      'DISEASE',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20.0, left: 150.0),
+                    child: Container(
+                      width: 50,
+                      height: 20,
+                      decoration: BoxDecoration(
+                          color: const Color(0xFF315ee7),
+                          borderRadius: BorderRadius.circular(2)),
+                    ),
+                  ),
                 ],
               ),
             ),
